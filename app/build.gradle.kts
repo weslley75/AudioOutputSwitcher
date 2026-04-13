b094749cfc8aed0a1a42555538d65b2cf5207a50
Weslley <weslleyalmeida6@gmail.com>
2026-04-13 19:09:59 -0300

    Bump version to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.wasystems.audiooutputswitcher"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
